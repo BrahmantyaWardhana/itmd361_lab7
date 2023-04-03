12cba88a4b2e05887038f64b5b2115f32ffcff23
Brahmantya Wardhana <bwardhana@hawk.iit.edu>
2023-04-02 19:56:38 -0500

    first attempt

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 function init(){
-//add your javascrip between these two lines of code
- 
+//add your javascript between these two lines of code
+  var button = document.getElementById('entrybutton');
+  
+  function textFunction(){
+    var textbook = document.getElementById('entryinput');
+    document.getElementById('textoutput').innerHTML = textbox.value;
+    alert("Brahmantya B. Wardhana " + textbox.value);
+  }
 
-
-
-
-
-
-
+  button.addEventListener('click', textFunction);
+  }
 window.addEventListener('load', init);
