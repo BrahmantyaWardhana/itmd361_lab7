804a9edc96aab8f5ccd040cd0c69e86df9fa2be4
Brahmantya Wardhana <bwardhana@hawk.iit.edu>
2023-04-02 20:12:44 -0500

    update js

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@ function init(){
 //add your javascript between these two lines of code
   var button = document.getElementById('entrybutton');
   
-  function alertText(){
+  function showMeText(){
     var textbook = document.getElementById('entryinput');
     document.getElementById('textoutput').innerHTML = textbox.value;
     alert("Brahmantya B. Wardhana " + textbox.value);
   }
 
-  button.addEventListener('click', alertText);
+  button.addEventListener('click', showMeText);
   }
   
 window.addEventListener('load', init);
